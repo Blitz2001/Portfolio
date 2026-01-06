e50966356e024fab926405e071bf4e7a03f6a0e3
THANAYAMWATTA D.M.A.S.B <IT23839656@my.sliit.lk>
2026-01-06 23:56:21 +0530

    fix: update base path for repo rename to Portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/PortfolioDEMO/',
+  base: '/Portfolio/',
   plugins: [react()],
   build: {
     target: 'esnext',
